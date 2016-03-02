38a3d2131ec9d9771549d0f56589a571af90867d
tPg <thatPowderGuy@gmail.com>
2016-03-02 17:37:52 +0100

    added animated STEMPEL that follows the mouse

---
+++ Assets/Playground/MouseFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFollow : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 wantedPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10f));
+        transform.position = wantedPos;
+	}
+}
