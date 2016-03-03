089e9dcb0726d24cc8359ab827e377920ac8d055
tPg <thatPowderGuy@gmail.com>
2016-03-03 16:08:15 +0100

    sent 007 to shadow the STEMPULUS

---
+++ Assets/Playground/FollowLikeAShadow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowLikeAShadow : MonoBehaviour {
+
+    public Transform followMe;
+
+    SpriteRenderer sren;
+
+    Vector3 defaultOffset;
+    float minAlpha;
+
+	// Use this for initialization
+	void Start () {
+
+        sren = gameObject.GetComponent<SpriteRenderer>();
+        defaultOffset = transform.position - followMe.position;
+        minAlpha = sren.color.a;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(followMe.position.x + defaultOffset.x, transform.position.y, transform.position.z);
+        float dy = (transform.position.y - followMe.position.y) / defaultOffset.y;
+        sren.color = new Color(sren.color.r, sren.color.g, sren.color.b, minAlpha * dy + (1.25f - dy));
+	}
+}
--- Assets/Playground/Stempel.cs
+++ Assets/Playground/Stempel.cs
@@ -10,7 +10,7 @@ public class Stempel : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
