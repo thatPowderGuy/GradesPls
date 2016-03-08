b117f97f5962228897b2ba6985b49e95fd76f2cf
tPg <thatPowderGuy@gmail.com>
2016-03-08 17:30:57 +0100

    modified camera movement
    there're two modes now.

--- Assets/Playground/CameraMovement.cs
+++ Assets/Playground/CameraMovement.cs
@@ -7,44 +7,115 @@ public class CameraMovement : MonoBehaviour {
     public Transform TopRightBound;
     public Transform BotLeftBound;
 
-    public AnimationCurve xDistToSpeed;
-    public AnimationCurve yDistToSpeed;
-    public float maxCamSpeedX;
-    public float maxCamSpeedY;
+    public AnimationCurve distToSpeed;
+    public float maxCamSpeed;
 
+    [Range(0,1)]
+    public int moveMethod;
+
+    Vector3 dir = new Vector3(0.15f, 0.1f, 0.025f);
+    float factor = 0.2f;
+	
 	// Use this for initialization
 	void Start () {
-	
+        dir.x *= Camera.main.pixelWidth;
+        dir.y *= Camera.main.pixelWidth;
+        dir.z *= Camera.main.pixelWidth;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+        if (moveMethod == 0)
+            MouseFollowWorldSpace();
+        else
+            MouseFollowScreenSpace();
+	}
+	
+	private void MouseFollowScreenSpace()
+	{
+        Vector3 dv = new Vector3();
+        if(Input.mousePosition.x < dir.z)
+        {
+            //move left 1
+            dv.x = -maxCamSpeed;
+        }
+        else if (Input.mousePosition.x < dir.x)
+        {
+            //move left .5
+            dv.x = -maxCamSpeed * factor;
+        }
+        else if (Input.mousePosition.x > (Camera.main.pixelWidth - dir.z))
+        {
+            //move right 1
+            dv.x = maxCamSpeed;
+        }
+        else if(Input.mousePosition.x > (Camera.main.pixelWidth - dir.x))
+        {
+            //move right .5
+            dv.x = maxCamSpeed * factor;
+        }
+
+        if(Input.mousePosition.y < dir.z)
+        {
+            //move down 1
+            dv.y = -maxCamSpeed;
+        }
+        else if(Input.mousePosition.y < dir.y)
+        {
+            //move down .5
+            dv.y = -maxCamSpeed * factor;
+        }
+        else if(Input.mousePosition.y > (Camera.main.pixelHeight - dir.z))
+        {
+            //move up 1
+            dv.y = maxCamSpeed;
+        }
+        else if(Input.mousePosition.y > (Camera.main.pixelHeight - dir.y))
+        {
+            //move up .5
+            dv.y = maxCamSpeed * factor;
+        }
+
+        transform.position = clampToBounds(transform.position + dv * Time.deltaTime);
+        
+	}
+	
+	private void MouseFollowWorldSpace()
+	{
         Vector3 v = Input.mousePosition;
         Vector3 wantedPos = Camera.main.ScreenToWorldPoint(new Vector3(v.x, v.y, 10f));
         wantedPos.z = transform.position.z;
        //wantedPos = clampToBounds(wantedPos);
 
         v = wantedPos - transform.position;
-        if (v.x > maxCamSpeedX)
-            wantedPos.x = transform.position.x + maxCamSpeedX;
+        if (v.x > maxCamSpeed)
+            wantedPos.x = transform.position.x + maxCamSpeed;
         else if (v.x < 0)
         {
-            if (v.x < -maxCamSpeedX)
-                wantedPos.x = transform.position.x - maxCamSpeedX;
+            if (v.x < -maxCamSpeed)
+                wantedPos.x = transform.position.x - maxCamSpeed;
             v.x = -v.x;
         }
 
-        if (v.y > maxCamSpeedY)
-            wantedPos.y = transform.position.y + maxCamSpeedY;
+        if (v.y > maxCamSpeed)
+            wantedPos.y = transform.position.y + maxCamSpeed;
         else if (v.y < 0)
         {
-            if (v.y < -maxCamSpeedY)
-                wantedPos.y = transform.position.y - maxCamSpeedY;
+            if (v.y < -maxCamSpeed)
+                wantedPos.y = transform.position.y - maxCamSpeed;
             v.y = -v.y;
         }
 
-        float dx = xDistToSpeed.Evaluate(v.x / maxCamSpeedX) * Time.deltaTime;
-        float dy = yDistToSpeed.Evaluate(v.y / maxCamSpeedY) * Time.deltaTime;
+        if (v.x < factor * maxCamSpeed)
+            v.x = 0;
+        else
+            v.x -= factor * maxCamSpeed;
+
+        float dx = distToSpeed.Evaluate(v.x / maxCamSpeed) * Time.deltaTime;
+        float dy = distToSpeed.Evaluate(v.y / maxCamSpeed) * Time.deltaTime;
+
+        wantedPos.x *= 0.8f;
 
         wantedPos.x = wantedPos.x * dx + transform.position.x * (1 - dx);
         wantedPos.y = wantedPos.y * dy + transform.position.y * (1 - dy);
